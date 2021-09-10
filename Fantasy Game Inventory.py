66048aecf697339bb01abc6313529bd2da9aea68
Matthieu Van Wittenberghe <mvanwit@gmail.com>
2021-09-10 08:31:52 +0200

    Update Fantasy Game Inventory.py
    added addToInventory function to handle a Loot List

--- Fantasy Game Inventory.py
+++ Fantasy Game Inventory.py
@@ -1,6 +1,7 @@
 #Fantasy Game Inventory.py
 
 exampleInv = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+emptyInv = {}
 
 def displayInventory(someInv):
     print('Inventory:')
@@ -14,3 +15,18 @@ def displayInventory(someInv):
     print('Total number of items: ' + str(totalItems))
 
 displayInventory(exampleInv)
+
+
+## List to Dictionary Function - Dragon Loot !
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+
+def addToInventory(someInv, someLoot):
+
+    for item in someLoot:
+        if item in someInv.keys():
+            someInv[item] += 1 #Increment quantity if item already exist in inv
+        else: 
+            someInv.setdefault(item, 1) #Create item in inv if doesnt already exist
+
+    print('Updated ', end='')
+    displayInventory(someInv)
